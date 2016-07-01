dac251bfc20c48fc5f8a5e131514e66cb63fbc98
Ing. Alejandro Rosales Martíez <alesito500@gmail.com>
2016-06-30 22:22:23 -0500

    99% de avance
    El avance se detuvo por las modificaciones solicitadas por el
    usuario.

--- html/contacto.php
+++ html/contacto.php
@@ -4,13 +4,15 @@
 	$tel=$_POST['tel'];
 	$listasucursales=$_POST['listasucursales'];
 	
-	$mensaje="Nombre: $nombre<br>Telefono: $tel<br>Sucursal: $listasucursales";
+	$mensaje="Nombre: $nombre<br>Telefono: $tel<br>Sucursal: $listasucursales correo de contacto $mail";
 	$titulo="Cotizacion de la pagina de internet";
 	
-	$contacto="contacto@autodinero.com.mx";
-	$cabeceras = "From: $mail" . "\r\n" .
+	$contacto="borregoebc@gmail.com";
+	$cabeceras = "From: pagina@autodinero.com.mx" . "\r\n" .
     "Reply-To: $mail" . "\r\n";
 	
 	mail($contacto, $titulo, $mensaje, $cabeceras);
+			
+	echo "Se envio el correo. Gracias";
 
 ?>
